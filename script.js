3d21cc1f4f6f73636f9ef21dbdfeb4a6022de0f4
Ayrekah <erica@ericabarnett.com>
2026-03-07 17:06:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-function orderUpdateCallback(data) {
-    if (data) {
-        // This log is your best friend right now. 
-        // If 'data' is empty, the library isn't connecting to the storefront correctly.
-        console.log("FastSpring Session Updated:", data);
-    }
-}
-
-console.log("Ready To Roll Store initialized.");
+// All logic is currently handled by FastSpring SBL HTML Attributes.
+console.log("Store Builder Library is active.");
