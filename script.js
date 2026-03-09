1c2cff11109e76c2db3021efeb9c9778f86d5461
Ayrekah <erica@ericabarnett.com>
2026-03-08 23:45:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // All logic is currently handled by FastSpring SBL HTML Attributes.
 console.log("Store Builder Library is active.");
-document.querySelector("#fsc-embedded-checkout-container").style.display = "block";
+document.addEventListener("click", function(e){
+
+  if(e.target.classList.contains("add-btn")){
+    document.getElementById("fsc-embedded-checkout-container").style.display = "block";
+  }
+
+});
