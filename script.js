4978507840cb380e233da22fb36aecdc52f6ea48
Ayrekah <erica@ericabarnett.com>
2026-03-10 12:01:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,13 @@
-/* PROMO CODE */
-
 function applycoupon() {
   var couponid = document.getElementById('couponcode').value;
   fastspring.builder.promo(couponid);
 }
 
+function markupHelpersCallback(){}
 
-/* HANDLEBARS HELPER */
+function dataCallback(data){
 
-function markupHelpersCallback() {
-
-  Handlebars.registerHelper('iff', function (lvalue, operator, rvalue, options) {
-
-    var operators = {
-      '==': function(l,r){ return l == r; },
-      '===': function(l,r){ return l === r; },
-      '!=': function(l,r){ return l != r; },
-      '<': function(l,r){ return l < r; },
-      '>': function(l,r){ return l > r; },
-      '<=': function(l,r){ return l <= r; },
-      '>=': function(l,r){ return l >= r; }
-    };
-
-    if (!operators[operator]) {
-      return options.inverse(this);
-    }
-
-    var result = operators[operator](lvalue,rvalue);
-
-    if(result){
-      return options.fn(this);
-    } else {
-      return options.inverse(this);
-    }
-
-  });
-
-}
-
-
-/* CART RENDERING */
-
-function dataCallback(data) {
+  console.log("FastSpring cart data:", data);
 
   var template = Handlebars.compile(
     document.getElementById("fsb-cart-template").innerHTML
@@ -51,11 +17,6 @@ function dataCallback(data) {
 
 }
 
-
-/* BASIC ERROR HANDLING */
-
-function errorCallback(code, message) {
-
-  console.error("FastSpring Error:", code, message);
-
+function errorCallback(code,message){
+  console.log("FastSpring error:",code,message);
 }
